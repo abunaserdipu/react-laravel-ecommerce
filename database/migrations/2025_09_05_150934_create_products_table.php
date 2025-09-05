9ff8a6cffa0afde41876d608546d8a864d8171c4
abunaserdipu <abunaserdipu0@gmail.com>
2025-09-05 21:48:48 +0600

    :chart_with_upwards_trend: Defined columns of the products table through the migration

--- database/migrations/2025_09_05_150934_create_products_table.php
+++ database/migrations/2025_09_05_150934_create_products_table.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +13,21 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 2000);
+            $table->string('slug', 2000);
+            $table->longText('description');
+            $table->foreignId('department_id')
+                ->index()
+                ->constrained('departments');
+            $table->foreignId('category_id')
+                ->index()
+                ->constrained('categories');
+            $table->decimal('price', 20, 4);
+            $table->string('status')->index();
+            $table->integer('quantity')->nullable();
+            $table->foreignIdFor(User::class, 'created_by');
+            $table->foreignIdFor(User::class, 'updated_by');
+            $table->timestamp('deleted_at')->nullable();
             $table->timestamps();
         });
     }
