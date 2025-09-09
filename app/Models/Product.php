3225a7edbbd51cdf8e46fee97845424ce9c627a2
abunaserdipu <abunaserdipu0@gmail.com>
2025-09-09 11:44:02 +0600

    :chart_with_upwards_trend: Add media library conversion which is three conversion thumb,small&large Add queued with conversion so this can happen in the background if need extra time Solve the issue of running queue by add  & =true in the model Also add the after_commit true in the queue config

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,11 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class Product extends Model implements HasMedia
 {
     use InteractsWithMedia;
 
+    public $queueable = true;
+
+    protected $dispatchAfterCommit = true; // ✅ ensures all dispatched jobs wait until after the transaction is committed
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this->addMediaConversion('thumb')
+            ->width(100)
+            ->queued(); // ✅ mark this conversion as queued;
+
+        $this->addMediaConversion('small')
+            ->width(480)
+            ->queued();
+
+        $this->addMediaConversion('large')
+            ->width(1200)
+            ->queued();
+    }
     public function department(): BelongsTo
     {
         return $this->belongsTo(Department::class);
