fe7168a932d34771f0ad697845c0ab9b319b5ae7
abunaserdipu <abunaserdipu0@gmail.com>
2025-09-19 16:32:11 +0600

    :chart_with_upwards_trend: Product details page develop ongoing

--- resources/js/pages/Product/Show.tsx
+++ resources/js/pages/Product/Show.tsx
@@ -1,5 +1,8 @@
+import Carousel from "@/Components/Core/Carousel";
+import CurrencyFormatter from "@/Components/Core/CurrencyFormatter";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Product, VariationTypeOption } from "@/types";
-import { useForm, usePage } from "@inertiajs/react";
+import { Head, router, useForm, usePage } from "@inertiajs/react";
 import { useEffect, useMemo, useState } from "react";
 
 export default function Show({
@@ -62,8 +65,119 @@ export default function Show({
                 false
             )
         }
-    })
-    return (
+    },[]);
 
+    const getOptionIdMap = (newOptions:object) => {
+        return Object.fromEntries(
+            Object.entries(newOptions).map(([a,b]) => [a,b.id])
+        )
+    };
+
+    const chooseOption = (
+        typeId: number,
+        option: VariationTypeOption,
+        updateRouter: boolean = true
+    ) => {
+        setSelectedOptions((prevSelectedOptions) => {
+            const newOptions = {
+                ...prevSelectedOptions,
+                [typeId]: option
+            }
+
+            if(updateRouter){
+                router.getUrl(url,{
+                    options: getOptionIdMap(newOptions)
+                },{
+                    preserveScroll: true,
+                    preserveState: true
+                })
+            }
+            return newOptions;
+        })
+    }
+
+    const onQuantityChange = (ev: React.ChangeEvent<HTMLSelectElement>) => {
+        form.setData('quantity',parseInt(ev.target.value));
+    }
+
+    const addToCart = () => {
+        form.post(route('cart.store',product.id),{
+            preserveScroll: true,
+            preserveState: true,
+            onError: (err) => {
+                console.log(err);
+            }
+        })
+    }
+
+    const renderProductVariationTypes = () => {
+        return(
+            product.variationTypes.map((type,i)=>(
+                <div key={type.id}>
+                    <b>{type.name}</b>
+                    {type.type === 'image' && 
+                    <div className="flex gap-2 mb-4">
+                        {type.options.map(option => (
+                            <div onClick={() => chooseOption(type.id,option)} key={option.id}>
+                                {option.images && <img src={option.images[0].thumb} alt="" className={'w-[50px]' +}/>}
+                            </div>
+                        ))}
+                    </div>
+                    }
+                </div>
+            ))
+        )
+    }
+
+    const renderAddToCartButton = () => {
+        return(
+
+        )
+    }
+
+    useEffect(() => {
+        const idsMap = Object.fromEntries(
+            Object.entries(selectedOptions).map(([typeId,option]:[string,VariationTypeOption]) => [typeId,option.id])
+        )
+        console.log(idsMap)
+        form.setData('option_ids',idsMap)
+
+    },[selectedOptions]);
+    return (
+        <AuthenticatedLayout>
+            <Head title="Home" />
+            <div className="container mx-auto p-8">
+                <div className="grid gap-8 grid-cols-1 lg:grid-cols-12">
+                    <div className="col-span-7">
+                        <Carousel images={images}/>
+                    </div>
+                    <div className="col-span-5">
+                        <h1 className="text-2xl mb-8">{product.title}</h1>
+
+                        <div>
+                            <div className="text-3xl-text-semibold">
+                                <CurrencyFormatter amount={computedProduct.price} />
+                            </div>
+                        </div>
+
+                        {renderProductVariationTypes()}
+
+                        {computedProduct.quantity != undefied &&
+                         computedProduct.quantity < 10 &&
+                         <div className="text-error my-4">
+                            <span>Only {computedProduct.quantity} left</span>
+                         </div>
+                         }
+                         {renderAddToCartButton()}
+
+                         <b className="text-xl">About the Item</b>
+                         <div className="wysiwyg-item"
+                         dangerouslySetInnerHTML={{__html: product.description}}>
+
+                         </div>
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
     );
 }
