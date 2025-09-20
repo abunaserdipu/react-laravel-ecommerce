4e3fa97221f67b09b2ddb6706c5320bdf3d9d544
abunaserdipu <abunaserdipu0@gmail.com>
2025-09-20 21:45:22 +0600

    :chart_with_upwards_trend: Define the renderAddToCartButton

--- resources/js/pages/Product/Show.tsx
+++ resources/js/pages/Product/Show.tsx
@@ -13,64 +13,69 @@ export default function Show({
     variationOptions: number[];
 }) {
     const form = useForm<{
-        option_ids: Record<string,number>;
+        option_ids: Record<string, number>;
         quantity: number;
         price: number | null;
     }>({
         option_ids: {},
         quantity: 1,
-        price: null // Populate price on change
-    })
+        price: null, // Populate price on change
+    });
 
     const url = usePage();
 
-    const [selectedOptions,setSelectedOptions] = useState<Record<number,VariationTypeOption>>([]);
+    const [selectedOptions, setSelectedOptions] = useState<
+        Record<number, VariationTypeOption>
+    >([]);
 
-    const images = useMemo(()=>{
-        for(let typeId in selectedOptions){
+    const images = useMemo(() => {
+        for (let typeId in selectedOptions) {
             const option = selectedOptions[typeId];
-            if(option.images.length > 0) return option.images;
+            if (option.image.length > 0) return option.image;
         }
         return product.images;
-    },[product,selectedOptions]);
+    }, [product, selectedOptions]);
 
-    const computedProduct = useMemo(()=>{
+    const computedProduct = useMemo(() => {
         const selectedOptionIds = Object.values(selectedOptions)
-        .map(op => op.id)
-        .sort();
+            .map((op) => op.id)
+            .sort();
 
-        for(let variation of product.variations){
-            const optionIds = variation.variation_type_option_ids.sort();
-            if(arraysAreEqual(selectedOptionIds,optionIds)){
-                return{
+        for (let variation of product.variations) {
+            const optionIds = variation.variation_type_option_id.sort();
+            if (arraysAreEqual(selectedOptionIds, optionIds)) {
+                return {
                     price: variation.price,
-                    quantity: variation.quantity === null ? Number.MAX_VALUE : variation.quantity,
-                }
-            
+                    quantity:
+                        variation.quantity === null
+                            ? Number.MAX_VALUE
+                            : variation.quantity,
+                };
             }
             return {
                 price: product.price,
                 quantity: product.quantity,
-            }
+            };
         }
-    },[product,selectedOptions]);
+    }, [product, selectedOptions]);
 
-    useEffect(()=>{
-        for(let type of product.variationTypes){
-            const selectedOptionId:number = variationOptions[type.id];
-            console.log(selectedOptionId,type.option);
+    useEffect(() => {
+        for (let type of product.variationTypes) {
+            const selectedOptionId: number = variationOptions[type.id];
+            console.log(selectedOptionId, type.option);
             chooseOption(
                 type.id,
-                type.option.find(op => op.id == selectedOptionId) || type.option[0],
+                type.option.find((op) => op.id == selectedOptionId) ||
+                    type.option[0],
                 false
-            )
+            );
         }
-    },[]);
+    }, []);
 
-    const getOptionIdMap = (newOptions:object) => {
+    const getOptionIdMap = (newOptions: object) => {
         return Object.fromEntries(
-            Object.entries(newOptions).map(([a,b]) => [a,b.id])
-        )
+            Object.entries(newOptions).map(([a, b]) => [a, b.id])
+        );
     };
 
     const chooseOption = (
@@ -81,117 +86,162 @@ export default function Show({
         setSelectedOptions((prevSelectedOptions) => {
             const newOptions = {
                 ...prevSelectedOptions,
-                [typeId]: option
-            }
+                [typeId]: option,
+            };
 
-            if(updateRouter){
-                router.getUrl(url,{
-                    options: getOptionIdMap(newOptions)
-                },{
-                    preserveScroll: true,
-                    preserveState: true
-                })
+            if (updateRouter) {
+                router.get(
+                    url,
+                    {
+                        options: getOptionIdMap(newOptions),
+                    },
+                    {
+                        preserveScroll: true,
+                        preserveState: true,
+                    }
+                );
             }
             return newOptions;
-        })
-    }
+        });
+    };
 
     const onQuantityChange = (ev: React.ChangeEvent<HTMLSelectElement>) => {
-        form.setData('quantity',parseInt(ev.target.value));
-    }
+        form.setData("quantity", parseInt(ev.target.value));
+    };
 
     const addToCart = () => {
-        form.post(route('cart.store',product.id),{
+        form.post(route("cart.store", product.id), {
             preserveScroll: true,
             preserveState: true,
             onError: (err) => {
                 console.log(err);
-            }
-        })
-    }
+            },
+        });
+    };
 
     const renderProductVariationTypes = () => {
-        return(
-            product.variationTypes.map((type,i)=>(
-                <div key={type.id}>
-                    <b>{type.name}</b>
-                    {type.type === 'image' && 
+        return product.variationTypes.map((type, i) => (
+            <div key={type.id}>
+                <b>{type.name}</b>
+                {type.type === "image" && (
                     <div className="flex gap-2 mb-4">
-                        {type.options.map(option => (
-                            <div onClick={() => chooseOption(type.id,option)} key={option.id}>
-                                {option.images && <img src={option.images[0].thumb} alt="" className={'w-[50px]' + (
-                                    selectedOptions[type.id]?.id === option.id ?
-                                    'outline outline-4 outline-primary' : ''
-                                )}/>}
+                        {type.option.map((option) => (
+                            <div
+                                onClick={() => chooseOption(type.id, option)}
+                                key={option.id}
+                            >
+                                {option.image && (
+                                    <img
+                                        src={option.image[0].thumb}
+                                        alt=""
+                                        className={
+                                            "w-[50px]" +
+                                            (selectedOptions[type.id]?.id ===
+                                            option.id
+                                                ? "outline outline-4 outline-primary"
+                                                : "")
+                                        }
+                                    />
+                                )}
                             </div>
                         ))}
-                    </div>}
-                    {type.type === 'radio' && 
+                    </div>
+                )}
+                {type.type === "radio" && (
                     <div className="flex join mb-4">
-                        {type.options.map(option => (
-                            <input onChange={() => chooseOption(type.id,option)} key={option.id} 
-                            className="join-item btn" 
-                            type="radio" 
-                            value={option.id} 
-                            checked={selectedOptions[type.id]?.id === option.id}
-                            name={"variation_type_" + type.id}
-                            aria-label={option.name}
+                        {type.option.map((option) => (
+                            <input
+                                onChange={() => chooseOption(type.id, option)}
+                                key={option.id}
+                                className="join-item btn"
+                                type="radio"
+                                value={option.id}
+                                checked={
+                                    selectedOptions[type.id]?.id === option.id
+                                }
+                                name={"variation_type_" + type.id}
+                                aria-label={option.name}
                             />
                         ))}
                     </div>
-                        }
-
-                </div>
-            ))
-        )
-    }
+                )}
+            </div>
+        ));
+    };
 
     const renderAddToCartButton = () => {
-        return(
-
-        )
-    }
+        return (
+            <div className="mb-8 flex gap-4">
+                <select
+                    value={form.data.quantity}
+                    onChange={onQuantityChange}
+                    className="select select-bordered w-full"
+                >
+                    {Array.from({
+                        length: Math.min(10, computedProduct.quantity),
+                    }).map((el, i) => (
+                        <option value={i + 1} key={i + 1}>
+                            Quantity: {i + 1}
+                        </option>
+                    ))}
+                </select>
+                <button onClick={addToCart} className="btn btn-primary">
+                    Add to Cart
+                </button>
+            </div>
+        );
+    };
 
     useEffect(() => {
         const idsMap = Object.fromEntries(
-            Object.entries(selectedOptions).map(([typeId,option]:[string,VariationTypeOption]) => [typeId,option.id])
-        )
-        console.log(idsMap)
-        form.setData('option_ids',idsMap)
-
-    },[selectedOptions]);
+            Object.entries(selectedOptions).map(
+                ([typeId, option]: [string, VariationTypeOption]) => [
+                    typeId,
+                    option.id,
+                ]
+            )
+        );
+        console.log(idsMap);
+        form.setData("option_ids", idsMap);
+    }, [selectedOptions]);
     return (
         <AuthenticatedLayout>
-            <Head title="Home" />
+            <Head title={product.title} />
             <div className="container mx-auto p-8">
                 <div className="grid gap-8 grid-cols-1 lg:grid-cols-12">
                     <div className="col-span-7">
-                        <Carousel images={images}/>
+                        <Carousel images={images} />
                     </div>
                     <div className="col-span-5">
                         <h1 className="text-2xl mb-8">{product.title}</h1>
 
                         <div>
                             <div className="text-3xl-text-semibold">
-                                <CurrencyFormatter amount={computedProduct.price} />
+                                <CurrencyFormatter
+                                    amount={computedProduct.price}
+                                />
                             </div>
                         </div>
 
                         {renderProductVariationTypes()}
 
                         {computedProduct.quantity != undefied &&
-                         computedProduct.quantity < 10 &&
-                         <div className="text-error my-4">
-                            <span>Only {computedProduct.quantity} left</span>
-                         </div>
-                         }
-                         {renderAddToCartButton()}
+                            computedProduct.quantity < 10 && (
+                                <div className="text-error my-4">
+                                    <span>
+                                        Only {computedProduct.quantity} left
+                                    </span>
+                                </div>
+                            )}
+                        {renderAddToCartButton()}
 
-                         <b className="text-xl">About the Item</b>
-                         <div className="wysiwyg-item"
-                         dangerouslySetInnerHTML={{__html: product.description}}>
-
-                         </div>
+                        <b className="text-xl">About the Item</b>
+                        <div
+                            className="wysiwyg-item"
+                            dangerouslySetInnerHTML={{
+                                __html: product.description,
+                            }}
+                        ></div>
                     </div>
                 </div>
             </div>
