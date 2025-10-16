d4c05556e21cc3235ec0accee46fd82099fc71c8
abunaserdipu <abunaserdipu0@gmail.com>
2025-10-16 12:35:30 +0600

    :chart_with_upwards_trend: Defined cart service methods, cart_items migration & create model & migration for vendor

--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -3,9 +3,16 @@
 namespace App\Services;
 
 use App\Models\Product;
+use Exception;
+use Illuminate\Support\Facades\Auth;
 
 class CartService
 {
+
+    private ?array $cachedCartItems = null;
+    protected const COOKIE_NAME = 'cartItems';
+    protected const COOKIE_LIFETIME = 60 * 24 * 365; // 1 year
+
     public function addItemToCart(Product $product, int $quantity = 1, $optionIds = null)
     {
 
@@ -17,6 +24,82 @@ class CartService
     }
 
     public function removeItemFromCart(int $productId, $optionIds = null)
+    {
+
+    }
+
+    public function getCartItems(): array
+    {
+        // We need to put this in try-catch, otherwise if something goes wrong,
+        // the website will not open at all
+        try {
+            if ($this->cachedCartItems === null) {
+                // If the user is authenticated, retrieve from the database
+                if (Auth::check()) {
+                    $cartItems = $this->getCartItemsFromDatabase();
+                } else {
+                    // If the user is a guest, retrive from cookies
+                    $cartItems = $this->getCartItemsFromCookies();
+                }
+
+                $productIds = collect($cartItems)->map(fn($item) => $item['produt_id']);
+                $products = Product::whereIn('id', $productIds)
+                    ->with('user.vendor');
+            }
+
+            return $this->cachedCartItems;
+
+        } catch (Exception $e) {
+
+        }
+    }
+
+    public function getTotalQuantity(): int
+    {
+
+    }
+
+    public function getTotalPrice(): float
+    {
+
+    }
+
+    protected function updateQuantityItemInDatabase(int $productId, int $quantity, array $optionIds): void
+    {
+
+    }
+
+    protected function updateItemQuantityInCookies(int $productId, int $quantity, array $optionIds): void
+    {
+
+    }
+
+    protected function saveItemToDatabase(int $productId, int $quantity, $price)
+    {
+
+    }
+
+    protected function saveItemToCookies(int $productId, int $quantity, $price)
+    {
+
+    }
+
+    protected function removeItemFromDatabase(int $productId, array $optionIds)
+    {
+
+    }
+
+    protected function removeItemFromCookies(int $productId, array $optionIds)
+    {
+
+    }
+
+    protected function getCartItemsFromDatabase()
+    {
+
+    }
+
+    protected function getCartItemsFromCookies()
     {
 
     }
--- database/migrations/2025_10_01_121803_create_cart_items_table.php
+++ database/migrations/2025_10_01_121803_create_cart_items_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,17 @@ return new class extends Migration
     {
         Schema::create('cart_items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('product_id')
+                ->index()
+                ->constrained('products')
+                ->cascadeOnDelete();
+            $table->foreignId('user_id')
+                ->index()
+                ->constrained('users');
+            $table->integer('quantity');
+            $table->decimal('price', 20, 4);
+            $table->json('variation_type_option_ids')->nullable();
+            $table->boolean('saved_for_later')->default('false');
             $table->timestamps();
         });
     }
